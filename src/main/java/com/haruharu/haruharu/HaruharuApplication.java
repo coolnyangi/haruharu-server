e2ae26b631d4ff754e4d1c6fb539b4ee8623cc6a
doo <kdh30166568@gmail.com>
2026-01-14 13:15:08 +0900

    Initial Commit

---
+++ src/main/java/com/haruharu/haruharu/HaruharuApplication.java
@@ -0,0 +1,13 @@
+package com.haruharu.haruharu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruharuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaruharuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haruharu/haruharu/HaruharuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haruharu.haruharu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaruharuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
