77e0841e446b8c5498e30928976d15b3786c6324
gwondoo <134582150+gwondoo@users.noreply.github.com>
2026-01-27 21:25:30 +0900

    feat : Initial commit

---
+++ src/main/java/com/haruharu/HaruharuApplication.java
@@ -0,0 +1,13 @@
+package com.haruharu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruharuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaruharuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haruharu/HaruharuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haruharu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaruharuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
