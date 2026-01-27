cda8bff8bb7747eff915812b3992f5f2b9f386ed
doo <kdh30166568@gmail.com>
2026-01-27 23:29:32 +0900

    update: BaseTimeEntity

--- src/main/java/com/haruharu/haruharu/common/entity/BaseTimeEntity.java
+++ src/main/java/com/haruharu/haruharu/common/entity/BaseTimeEntity.java
@@ -1,15 +1,25 @@
 package com.haruharu.haruharu.common.entity;
 
 import com.haruharu.haruharu.common.response.ApiResponse;
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(ApiResponse.class)
-
 public class BaseTimeEntity {
+
     @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
 }
