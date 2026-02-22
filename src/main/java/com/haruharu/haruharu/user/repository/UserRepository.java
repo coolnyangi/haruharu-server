5dd3fc7c7e3ac725dbb0b7501b1301a02cfe2603
doo <kdh30166568@gmail.com>
2026-02-22 15:55:21 +0900

    chore: UserRepository 오타 수정

--- src/main/java/com/haruharu/haruharu/user/repository/UserRepository.java
+++ src/main/java/com/haruharu/haruharu/user/repository/UserRepository.java
@@ -4,3 +4,4 @@ import com.haruharu.haruharu.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+}
